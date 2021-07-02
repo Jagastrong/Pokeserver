34859679856602516783b35eaadfd19064bcf9b2
Jagastrong <86830420+Jagastrong@users.noreply.github.com>
2021-07-02 15:49:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const botsettings = require('./botsettings.json');
+
+const bot = new Discord.Client({disableEveryone: true});
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online`)
+    bot.user.setActivity("PokeServers", {type: "WATCHING"});
+})
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hello`){
+        return message.channel.send("**Hello nanba**")
+    } 
+    {
+    if(cmd === `${prefix}hi`)
+        return message.reply("How are u nanba!")
+   }
+   if(cmd === `${prefix}clear`)
+   {if (message.deletable) { 
+       message.delete();
+       message.channel.send("**Messages have been cleared**") 
+       } 
+   if (!message.member.hasPermission("MANAGE_MESSAGES")) { 
+       return message.reply("Missing Permissions!").then(m => m.delete(5000));
+        } 
+        
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { 
+        return message.reply("This is not a number").then(m => m.delete(5000));
+         } 
+         
+         let deleteAmount; 
+         if (parseInt(args[0]) > 100) { 
+             deleteAmount = 100; 
+             } else { 
+                 deleteAmount = parseInt(args[0]); 
+                 } 
+                 message.channel.bulkDelete(deleteAmount, true) 
+                 .catch(err => message.reply(`Something went wrong... ${err}`)); }
+})
+bot.login(botsettings.token);
